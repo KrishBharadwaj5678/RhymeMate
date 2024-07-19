cfdc84a2d15230f8aac51b824125edea3641c9a7
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2024-07-19 17:27:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,19 +14,22 @@ st.write("<h2 style='color:orange;'>Find Rhymes for Any Word Instantly</h2>",uns
 rhyme=st.text_input("Enter a Word")
 btn=st.button("Generate")
 if btn:
-    word = f'{rhyme}'
-    api_url = 'https://api.api-ninjas.com/v1/rhyme?word={}'.format(word)
-    response = requests.get(api_url, headers={'X-Api-Key': '2jWCY0dASiPZc7RLybXvXA==R9oC0XPKPWiGJ6k6'})
-    if response.status_code == requests.codes.ok:
-       data=response.json()
-       if(len(data)==0):
-          st.error("No Rhymes for Your Word")
-       else:
-        st.write(f"<h3 style=color:#FF69B4;>Words that rhyme with {rhyme}:</h3>",unsafe_allow_html=True)
-        # Extracting Unique Rhymes
-        unique_rhymes=[]
-        for i in data:
-           if i not in unique_rhymes:
-              unique_rhymes.append(i)
-        for j in unique_rhymes:
-            st.write(f"""<li style=font-size:22px;>{j}</li>""",unsafe_allow_html=True)
+    try:
+        word = f'{rhyme}'
+        api_url = 'https://api.api-ninjas.com/v1/rhyme?word={}'.format(word)
+        response = requests.get(api_url, headers={'X-Api-Key': '2jWCY0dASiPZc7RLybXvXA==R9oC0XPKPWiGJ6k6'})
+        if response.status_code == requests.codes.ok:
+            data=response.json()
+            if(len(data)==0):
+                st.error("No Rhymes for Your Word")
+            else:
+                st.write(f"<h3 style=color:#FF69B4;>Words that rhyme with {rhyme}:</h3>",unsafe_allow_html=True)
+                # Extracting Unique Rhymes
+                unique_rhymes=[]
+                for i in data:
+                    if i not in unique_rhymes:
+                        unique_rhymes.append(i)
+                for j in unique_rhymes:
+                    st.write(f"""<li style=font-size:22px;>{j}</li>""",unsafe_allow_html=True)
+    except:
+        st.error("Network Error 🔌")
